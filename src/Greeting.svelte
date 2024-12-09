070837bc0129e41fb3f6175193f3f8a923412051
angelhien90 <162631460+angelhien90@users.noreply.github.com>
2024-12-09 16:13:43 -0600

    Created sv components: greeting, header

---
+++ src/Greeting.svelte
@@ -0,0 +1,104 @@
+<script>
+    let name = 'Greeting';
+    
+	import pinkCircle from '../lib/images/GreetingCircle_Pink.png'
+    import yellowCircle from '../lib/images/GreetingCircle_Yellow.png'
+    import purpleCircle from '../lib/images/GreetingCircle_Purple.png'
+</script>
+
+<app id="greeting">
+    <div class="horizAlign two">
+        <div class="item">howdy!</div>
+        <div class="item">i'm angel le.</div>
+    </div>
+
+    <div class="horizAlign four">
+        <div class="item">i'm a 2D</div>
+        <div class="item_img"><a href="/2DProject" ><img src={purpleCircle} width=45% alt="purple circle"></a></div>
+        <div class="item">, 3D</div>
+        <div class="item_img"><a href="/3DProject" ><img src={yellowCircle} width=45% alt="yellow circle"></a>,</div>
+    </div>
+
+    <div class="horizAlign four">
+        <div class="item">and layout</div>
+        <div class="item_img"><a href="/layoutProject" ><img src={pinkCircle} width=45% alt="pink circle"></a></div>
+        <div class="item">artist from</div>
+        <div class="item">texas a&m uni.</div>
+    </div>
+    
+    <div class="horizAlign two">
+        <div class="item" id="smallText">check out my work below!</div>
+    </div>
+
+</app>
+
+<style>
+    #greeting {
+        background-color: var(--light);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        min-height: 100vh;
+        width: 80vw;
+    }
+
+    .horizAlign {
+        display: inline-block;
+        vertical-align: middle;
+        width: 95%;
+        height: auto;
+        padding: 0px 10px 0px 10px;
+        margin: 0% 2% 0% 2%;
+        border: 1px solid transparent;
+        
+        &.two .item, 
+        &.four .item {width: auto;}
+
+        .item {
+            display: inline-block;
+            vertical-align: middle;
+            height: auto;
+            min-height: 100px;
+            padding-right: 20px;
+            box-sizing: border-box;			
+            background: transparent;
+
+            font-size: 72px;
+            font-weight: 800;
+            line-height: auto;
+            opacity: 90%;
+        }
+        .item_img {
+            display: inline-block;
+            vertical-align: middle;
+            height: auto;
+            min-height: 100px;
+            padding-right: 20px;
+            box-sizing: border-box;			
+            background: transparent;
+
+            font-size: 72px;
+            font-weight: 800;
+            line-height: auto;
+            opacity: 90%;
+                
+            &:hover,
+            &:focus {
+                opacity: 60%;
+                scale: 1.2;
+            }
+        }
+    }
+
+    #smallText {
+        font-family: "jost", sans-serif;
+        font-weight: 500;
+        font-size: 24pt;
+        text-align: left;
+        padding: 0px 40px 40px 40px;
+        color: var(--dark);
+        opacity: 60%;
+    }
+
+</style>
+ 
---
+++ src/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+	// import { page } from '$app/stores';
+	let name = 'Header';
+</script>
+
+<header>
+	<div class="navBar">
+        <nav>
+			<a id="homepage" href="/">angel le</a>
+		</nav>
+
+        <nav> 
+            <a class="button" href="#aboutSection">about</a>
+        </nav>
+    </div>
+</header>
+
+<style>
+	#homepage{
+		font-family: "jost", sans-serif;
+		font-weight: 800;
+		font-size: 36pt;
+		color: var(--light);
+		padding: 20px;
+		margin-left: 20px;
+
+		&:hover,
+		&:focus {
+			color: var(--blueM);}
+	}
+</style>
