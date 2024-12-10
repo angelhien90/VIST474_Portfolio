5de004729a148fa2da290d254cb9a43581bc893a
angelhien90 <162631460+angelhien90@users.noreply.github.com>
2024-12-09 19:21:03 -0600

    Set up responsive/homepage work grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,20 @@
 	import Header from '../Header.svelte';
     import Footer from '../Footer.svelte';
 	import '../app.css';
+	import WorkGrid from '../workGrid.svelte';
 
 </script>
 
 <Greeting></Greeting>
+
+<main class="app_02">
+	<div class="tagBox">
+		<h1>
+			testing LEFT side
+		</h1>
+		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis ipsum itaque rem, labore enim, officiis consequatur laborum saepe similique aperiam alias, velit sint. Nisi repellendus exercitationem consequuntur, minima sed accusamus.</p>
+	</div>
+
+	<WorkGrid></WorkGrid>
+
+</main>
---
+++ src/workGrid.svelte
@@ -0,0 +1,37 @@
+<script> 
+	
+	import taggedSigns_art from '../src/lib/images/TaggedSigns_ConceptArt.jpg';
+    import wrongTree from '../src/lib/images/WrongTree_AssetLineup_Stage_3.png';
+    import zen from '../src/lib/images/Zen_Shot_01.png';
+    import soulBarbershop from '../src/lib/images/SoulBarbershop_Shot_01.png';
+    import rttw_featured from '../src/lib/images//RTTW_FeaturedShot.png';
+    import teapot from '../src/lib/images/Teapot_Frame_01.png';
+
+</script>
+
+<main>
+
+    <div><a href="/2DProject" ><img class="work_img" src={taggedSigns_art} alt="2DProject - Tagged Signs Concept Art"></a></div>
+
+    <div><a href="/3DProject" ><img class="work_img" src={wrongTree} alt="3DProject - Wrong Tree Asset Lineup"></a></div>
+
+    <div><a href="/layoutProject" ><img class="work_img" src={zen} alt="Layout Project - Zen Environment"></a></div>
+    
+    <div><a href="/soulBarbershopScene" ><img class="work_img" src={soulBarbershop} alt="3D Recreation - Soul Barbershop"></a></div>
+
+    <div><a href="/rttwGame" ><img class="work_img" src={rttw_featured} alt="Team Game - RTTW"></a></div>
+
+    <div><a href="/teapotAsset" ><img class="work_img" src={teapot} alt="3D Recreation Asset - Teapot"></a></div>
+
+</main>
+
+
+
+<style>
+    /*n/a*/
+    .work_img {
+        width: 20vw;
+        height: auto;
+    }
+</style>
+ 
